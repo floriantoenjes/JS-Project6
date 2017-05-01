0894c7a78091ef4d9d9630b7b18bcbab14fabdab
Florian Tönjes <skatepainter@yahoo.de>
2017-05-01 14:29:45 +0200

    Implemented promises for csv code to execute after the collecting of shirtPages

--- scraper.js
+++ scraper.js
@@ -17,21 +17,27 @@ scrapeIt(baseUrl + "/shirts.php", {
         }
     }
 }, (err, page) => {
-
+    const promises = [];
     for (let article of page.articles) {
-
-        scrapeIt(baseUrl + "/" + article.src, {
-            title: {
-                selector: "img"
-                , attr: "alt"
-            },
-            price: ".price",
-            imgUrl: {
-                selector: "img"
-                , attr: "src"
-            }
-        }, (er, shirtPage) => {
-            shirts.push(shirtPage);
-        });
+        promises.push(new Promise(function(resolve, reject){
+            scrapeIt(baseUrl + "/" + article.src, {
+                title: {
+                    selector: "img"
+                    , attr: "alt"
+                },
+                price: ".price",
+                imgUrl: {
+                    selector: "img"
+                    , attr: "src"
+                }
+            }, (er, shirtPage) => {
+                shirts.push(shirtPage);
+                console.log("First");
+                resolve(true);
+            });
+        }));
     }
+    Promise.all(promises).then(function() {
+        console.log("Last");
+    });
 });
