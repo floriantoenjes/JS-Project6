9cb4247cb0ade01b57e519f3b1e87eecf1e81b9e
Florian Tönjes <skatepainter@yahoo.de>
2017-05-01 10:44:09 +0200

    Started implementing first content scrape

---
+++ scraper.js
@@ -0,0 +1,56 @@
+const scrapeIt = require("scrape-it");
+
+scrapeIt("http://www.shirts4mike.com/shirts.php", {
+    // Fetch the articles
+    articles: {
+        listItem: ".products li"
+      , data: {
+
+            // Get the article date and convert it into a Date object
+            createdAt: {
+                selector: ".date"
+              , convert: x => new Date(x)
+            }
+
+            // Get the title
+          , src: {
+              selector: "a"
+              , attr: "href"
+          }
+
+            // Nested list
+          , tags: {
+                listItem: ".tags > span"
+            }
+
+            // Get the content
+          , content: {
+                selector: ".article-content"
+              , how: "html"
+            }
+        }
+    }
+
+    // Fetch the blog pages
+  , pages: {
+        listItem: "li.page"
+      , name: "pages"
+      , data: {
+            title: "a"
+          , url: {
+                selector: "a"
+              , attr: "href"
+            }
+        }
+    }
+
+    // Fetch some other data from the page
+  , title: ".header h1"
+  , desc: ".header h2"
+  , avatar: {
+        selector: ".header img"
+      , attr: "src"
+    }
+}, (err, page) => {
+    console.log(err || page);
+});
