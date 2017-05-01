b0ca619cc78e279d6b36efe7b0534c600f9b3c13
Florian Tönjes <skatepainter@yahoo.de>
2017-05-01 11:16:22 +0200

    Started scraping shirt pages

--- scraper.js
+++ scraper.js
@@ -1,60 +1,30 @@
+"use strict";
+
 const scrapeIt = require("scrape-it");
 
-scrapeIt("http://www.shirts4mike.com/shirts.php", {
+const baseUrl = "http://www.shirts4mike.com";
+
+scrapeIt(baseUrl + "/shirts.php", {
     // Fetch the articles
     articles: {
         listItem: ".products li"
       , data: {
-
-            // Get the article date and convert it into a Date object
-            createdAt: {
-                selector: ".date"
-              , convert: x => new Date(x)
-            }
-
-            // Get the title
-          , src: {
+            src: {
               selector: "a"
               , attr: "href"
           }
-
-            // Nested list
-          , tags: {
-                listItem: ".tags > span"
-            }
-
-            // Get the content
-          , content: {
-                selector: ".article-content"
-              , how: "html"
-            }
         }
-    }
-
-    // Fetch the blog pages
-  , pages: {
-        listItem: "li.page"
-      , name: "pages"
-      , data: {
-            title: "a"
-          , url: {
-                selector: "a"
-              , attr: "href"
-            }
-        }
-    }
-
-    // Fetch some other data from the page
-  , title: ".header h1"
-  , desc: ".header h2"
-  , avatar: {
-        selector: ".header img"
-      , attr: "src"
     }
 }, (err, page) => {
     console.log(err || page);
 
     for (let article of page.articles) {
         console.log(article.src);
+
+        scrapeIt(baseUrl + "/" + article.src, {
+            price: ".price"
+        }, (er, shirtPage) => {
+            console.log(er || shirtPage);
+        });
     }
 });
